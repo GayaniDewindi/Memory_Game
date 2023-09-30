d440fd450fce5f2e52ebbe251945024e59ecf2ec
Gayani Dewindi Vitharana <gayanidewindi@gmail.com>
2023-09-30 21:44:53 +0530

    script compled

--- 2D_Game/assets/js/script.js
+++ 2D_Game/assets/js/script.js
@@ -1,35 +1,37 @@
 var runStart =0;
-function Start (evevt){
+function Start (evevt) {
 
     //Enter key
 
-    if (evevt.which == 13){
-        if (runWorkerId == 0){
+    if (evevt.which == 13) {
+        if (runWorkerId == 0) {
             runWorkerId = setInterval(run, 100);
-            runStart =1;
+            runStart = 1;
             runSound.play();
             sound.play();
 
-            backgroundWorkeId = setInterval(moveBackground,100);
-            scoreWorkerId = setInterval(updateScore,100);
-            blockWorkerId = setInterval(createBlock,100);
-            moveBlockWorkerId=setInterval(moveBlocks,100);
+            backgroundWorkeId = setInterval(moveBackground, 100);
+            scoreWorkerId = setInterval(updateScore, 100);
+            blockWorkerId = setInterval(createBlock, 100);
+            moveBlockWorkerId = setInterval(moveBlocks, 100);
 
         }
     }
 
+
     //space key
-    if (evevt.which==32){
-        if (runStart==1){
-            if (jumpWorkerId == 0){
+    if (evevt.which == 32) {
+        if (runStart == 1) {
+            if (jumpWorkerId == 0) {
                 clearInterval(runWorkerId);
                 runSound.pause();
                 runWorkerId = -1;
-                jumpWorkerId = setInterval(jump,100);
+                jumpWorkerId = setInterval(jump, 100);
                 jumpSound.play();
             }
         }
     }
+}
 
     //Run Function
    var player=document.getElementById("player");
@@ -153,6 +155,24 @@ function Start (evevt){
             }
         }
 
+  // dead function
 
+        var deadImageNumber=1;
+        var deadWorkerId=0;
 
-}
+        var deadSound = new Audio("dead.mp3");
+        function dead(){
+            if(deadImageNumber ==1){
+                deadImageNumber=10;
+                player.style.marginTop="430px";
+
+                document.getElementById("GameOver").style.visibility="visible";
+                document.getElementById("endScore").innerHTML=scoreValue;
+            }
+            player.src="Dead("+deadImageNumber+").png";
+        }
+
+        function re(){
+            location.reload();
+        }
+
